8214f4a0a21d42345630d7eb7f035e0f5585d665
Shelton Fernandes <brucef1566@gmail.com>
2022-05-27 06:45:52 +0530

    added login activity

--- app/src/main/java/mad/project/hemkitem/SplashScreenActivity.java
+++ app/src/main/java/mad/project/hemkitem/SplashScreenActivity.java
@@ -2,7 +2,10 @@ package mad.project.hemkitem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Fullscreen
+        View decorView=getWindow().getDecorView();
+
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        //next page transition
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,LoginActivity.class));
+            }
+        },3000);
+
+
+
+
     }
 }
